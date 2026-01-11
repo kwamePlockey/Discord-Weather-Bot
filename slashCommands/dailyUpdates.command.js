0ee51a7e9daeee60b6997b790ff7347f4a72bd96
Emmanuel Kwame Plockey <emmaplockey123@gmail.com>
2026-01-11 18:30:41 +0000

    check city validity

--- slashCommands/dailyUpdates.command.js
+++ slashCommands/dailyUpdates.command.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getDailyWeatherUpdates } = require('../botFunctions/main');
 const {subscriptionMessage} = require('../botFunctions/controllers.js/constants')
+const cityExists = require('../botFunctions/controllers.js/fetchForecastAPI')
 
 const dailyUpdateSlashCommand = new SlashCommandBuilder()
   .setName('subscribe_forecast_daily')
@@ -21,11 +22,12 @@ module.exports = {
     .replace(/\.$/gi, '');
 
     try {
-        //check if city exists
+        if(await cityExists(city)){
         interaction.reply(subscriptionMessage.DAILY_WEATHER_UPDATES(city));
-        //const weatherData = await getDailyWeatherUpdates(city);
+        }
 
-      //set-up cron
+        //set-up cron
+        //const weatherData = await getDailyWeatherUpdates(city);
     } catch (error) {
       console.error(error)
       await interaction.reply(error.message);
