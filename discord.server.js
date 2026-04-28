76480758b51dd804b78e8e38a42823d3716e0261
Emmanuel Kwame Plockey <emmaplockey123@gmail.com>
2026-04-28 18:17:07 +0000

    Refactored cron schedule function

--- discord.server.js
+++ discord.server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const fs = require('node:fs');
-const { getDailyWeatherUpdates } = require('./botFunctions/main');
-const token  = process.env.DISCORD_TOKEN;
-const channel_id = process.env.CHANNEL_ID;
 const cron = require('node-cron');
 const dB = require('./mockDB');
-
-// Require the necessary discord.js classes
+const { getDailyWeatherUpdates } = require('./botFunctions/main');
+const {scheduledTimeForDailyForecastUpdates} = require('./botFunctions/utilities.js/constants');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+
+const token  = process.env.DISCORD_TOKEN;
+const channel_id = process.env.CHANNEL_ID;
+
 
 // Create a new client instance
 const client = new Client({ intents: [
@@ -26,6 +27,19 @@ fs.readdirSync(commandsFolder).map(file => {
 	client.commands.set(command.data.name, command)
 })
 
+//Function for cron scheduler 
+const pushDailyForecastUpdatesToChannel = async () => {
+	const channel = await client.channels.fetch(channel_id);
+	const UnresolvedForecastArrayOfEachCity = [...dB].map(city => {
+		return getDailyWeatherUpdates(city)
+	})	
+
+	const resolvedForecastArrayOfEachCity = await Promise.allSettled(UnresolvedForecastArrayOfEachCity);
+
+	return resolvedForecastArrayOfEachCity.forEach(forecast => channel.send(forecast.value));
+}
+
+
 //Command execution
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
@@ -40,29 +54,16 @@ client.on(Events.InteractionCreate, async interaction => {
 		console.error(error)
 		await interaction.reply({ content: 'An error occured while executing this command!'});
 	}
+});
 
-
-} );
 
 
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, async readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-	
 
-	cron.schedule( ("0 23 8 * * *"), async () => {
-		console.log([...dB])
-		const channel = await client.channels.fetch(channel_id);
-		const UnresolvedForecastArrayOfEachCity = [...dB].map(city => {
-			return getDailyWeatherUpdates(city)
-		} )	
-
-		const resolvedForecastArrayOfEachCity = await Promise.allSettled(UnresolvedForecastArrayOfEachCity);
-
-		return resolvedForecastArrayOfEachCity.forEach(forecast => channel.send(forecast.value));
-	})
-
-	
+	const timeString = Object.values(scheduledTimeForDailyForecastUpdates).join(' ');
+	cron.schedule((timeString), pushDailyForecastUpdatesToChannel);
 });
 
 
